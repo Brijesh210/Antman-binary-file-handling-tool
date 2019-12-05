e061b6dab0087acd8ca2c3efe35941ab5bc6977b
Ismoil Atajanov <ismail11270@gmail.com>
2019-12-05 15:45:09 +0100

    Decoder fixed and tested for lvl-4

--- test/org/antman/binaryconverter/application/converter/DecoderTest.java
+++ test/org/antman/binaryconverter/application/converter/DecoderTest.java
@@ -21,12 +21,32 @@ class DecoderTest {
     File decodedFileTwo = new File("test-data\\decoded-lvl2.txt");
     File encodedFileTwo = new File("test-data\\encoded-lvl2.dat");
     FileHandler handler = new FileHandler();
+
+    File structureFileFour = new File("test-data\\structure\\structure-lvl-4.txt");
+    File encodedFileFour = new File("test-data\\encoded\\encoded-lvl-4.dat");
+
     @Test
-    void decode() {
+    void decodeLvlTwo() {
         Decoder decoder = new Decoder();
         try {
             structure = BinaryStructure.getInstance(structureFileTwo);
             ByteBuffer buffer = handler.readBytesToBuffer(encodedFileTwo);
+            System.out.println(decoder.decode(structure, buffer));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (InvalidBinaryStructureException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    void decodeLvlFour() {
+        Decoder decoder = new Decoder();
+        try {
+            structure = BinaryStructure.getInstance(structureFileFour);
+            ByteBuffer buffer = handler.readBytesToBuffer(encodedFileFour);
             System.out.println(decoder.decode(structure, buffer));
         } catch (FileNotFoundException e) {
             e.printStackTrace();
